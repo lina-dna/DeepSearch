3d8d420a90c2071099a500ba416782203130c29f
yobi213 <yeop7747@gmail.com>
2021-07-15 14:52:01 +0900

    word_cloud

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -20,7 +20,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome("./etc/chromedriver", options = chrome_options)
+driver = webdriver.Chrome("chromedriver", options = chrome_options)
 
 # 기간 설정
 today = datetime.datetime.now()
